60d34b1124562774e6f8b9b6950ce86a04a1d1e9
Murtuza Khan <mohdmurtuzakhan@gmail.com>
2018-01-06 19:10:12 +0530

    Added empty mvc project

---
+++ mali/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mali.MvcApplication" Language="C#" %>
---
+++ mali/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mali.Startup))]
+namespace mali
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
